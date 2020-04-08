b88fecd7e934ea5bb5dc0a83a595b5c7567862eb
Potya <papotemkin@edu.hse.ru>
2020-04-08 11:40:52 +0300

    new try

--- src/main/java/io/ino/Handler.java
+++ src/main/java/io/ino/Handler.java
@@ -1,5 +1,8 @@
 package io.ino;
 
+import io.kubeless.Event;
+import io.kubeless.Context;
+
 public class Handler {
         public String sayHello(io.kubeless.Event event, io.kubeless.Context context) {
             System.out.println(event.toString());
