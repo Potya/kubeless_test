28f488481a310be6193686ed28a008794c94af94
Potya <papotemkin@edu.hse.ru>
2020-04-08 12:02:26 +0300

    justify added

--- src/main/java/io/ino/Handler.java
+++ src/main/java/io/ino/Handler.java
@@ -3,9 +3,12 @@ package io.ino;
 import io.kubeless.Event;
 import io.kubeless.Context;
 
+import org.leadpony.justify.api.JsonValidationService;
+
 public class Handler {
         public String sayHello(io.kubeless.Event event, io.kubeless.Context context) {
             System.out.println(event.toString());
+            JsonValidationService service = JsonValidationService.newInstance();
             return "Hello world! AFDFCH";
         }
     }
